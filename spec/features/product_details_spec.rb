1c1a4d49bc7b63ad02dfde0f76ab74eb0fb6dc89
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-08-25 23:32:42 +0000

    tests for home page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+    @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+    end
+  end
+
+  scenario "They see product details" do
+    # ACT
+    visit root_path
+
+    # hover over the first product and click details
+   first("article.product").click_on 'Details »'
+   expect(page).to have_content "Description"
+
+
+   # DEBUG / VERIFY
+   save_screenshot
+   
+
+  end
+
+
+end
